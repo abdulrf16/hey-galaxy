55ca52beaead64b8360434b1b5c5b8acc7430cc6
abdulrf16 <abdul.rf16@gmail.com>
2026-09-17 01:43:03 +0530

    build.gradle.kts

--- heygalaxy/app/build.gradle.kts
+++ heygalaxy/app/build.gradle.kts
@@ -53,5 +53,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.service)
     implementation(libs.material)
 }
